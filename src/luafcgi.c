fa1de228b9a5f41381be286d3c4b480af3146886
Christopher Tombleson <chris@cribznetwork.com>
2013-11-18 15:16:19 +1300

    Create New, getHost and getPort functions

--- src/luafcgi.c
+++ src/luafcgi.c
@@ -1,12 +1,48 @@
 #include "luafcgi.h"
 
 static int New(lua_State *L) {
-    return 0;
+    int arg_n = lua_gettop(L);
+
+    if (arg_n > 2) {
+        lua_pushstring(L, "luafcgi.New take 2 arguments");
+        lua_error(L);
+        return 0;
+    }
+
+    if (!lua_isstring(L, 1)) {
+        lua_pushstring(L, "luafcgi.New expects a string as the first argument");
+        lua_error(L);
+        return 0;
+    }
+
+    host = lua_tolstring(L, 1, NULL);
+
+    if (!lua_isnumber(L, 2)) {
+        lua_pushstring(L, "luafcgi.New expects a integer as the second argument");
+        lua_error(L);
+        return 0;
+    }
+
+    port = lua_tointeger(L, 2);
+
+    return 1;
+}
+
+static int getHost(lua_State *L) {
+    lua_pushstring(L, host);
+    return 1;
+}
+
+static int getPort(lua_State *L) {
+    lua_pushinteger(L, port);
+    return 1;
 }
 
 LUALIB_API int luaopen_luafcgi(lua_State *L) {
     struct luaL_Reg driver[] = {
         {"New", New},
+        {"getHost", getHost},
+        {"getPort", getPort},
         {NULL, NULL},
     };
 
--- src/luafcgi.h
+++ src/luafcgi.h
@@ -3,10 +3,12 @@
 #include "lualib.h"
 #include "lauxlib.h"
 
-#define LUA_LIB extern
-#define PROJECT_TABLENAME "luafcgi" extern
+#define LUA_LIB
+#define PROJECT_TABLENAME "luafcgi"
 
-extern char *host;
-extern int  port;
+const char* host;
+int  port;
 int LUA_API luaopen_luafcgi (lua_State *L);
 static int New(lua_State *L);
+static int getHost(lua_State *L);
+static int getPort(lua_State *L);
