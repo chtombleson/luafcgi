a4c01acf265ed1f2d454e6d44d3a3411dcdb0cf1
Christopher Tombleson <chris@cribznetwork.com>
2013-11-26 15:54:36 +1300

    Removed get.c and url.c these need to be handled by the application

--- src/functions/get.h
+++
@@ -1,9 +0,0 @@
-#ifndef LUAFCGI_FUNCTIONS_GET_H
-#define LUAFCGI_FUNCTIONS_GET_H
-typedef struct {
-    char *key, *value;
-} getParam;
-
-getParam* parseQueryString(char *query_string);
-void getVar();
-#endif
--- src/functions/url.h
+++
@@ -1,7 +0,0 @@
-#ifndef LUAFCGI_FUNCTIONS_URL_H
-#define LUAFCGI_FUNCTIONS_URL_H
-char from_hex(char ch);
-char to_hex(char code);
-char *urlencode(char *str);
-char *urldecode(char *str);
-#endif
